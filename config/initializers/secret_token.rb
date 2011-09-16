02849d2d7675e32f2b3933649da7759b0a22b178
Joshua Blount <hello@joshuablount.com>
2011-09-16 01:24:46 -0400

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freebieber::Application.config.secret_token = 'b9a9c84dfb838f98da367722042efaa352c770a431793bc11d1e86d9e9c329d824b89672a9e9fb87f1926aa1e79b84d0403bb7491a47692a44dc9ff25de45420'
